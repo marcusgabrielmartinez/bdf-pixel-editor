f591621701e01074f6d73887c7a55dc8ec4b145e
Marcus Gabriel Martinez <marcusgabrielmartinez@users.noreply.github.com>
2021-09-26 03:55:36 -0700

    Added logic to select and deselect pixels.

---
+++ interface.js
@@ -0,0 +1,45 @@
+// Build 2D array for storing pixel data
+let pixels = [];
+var i;
+for (i=0; i<16; i++) {
+    var j;
+    pixels.push([]);
+    for (j=0; j<16; j++) {
+        pixels[i].push("0");
+    }
+}
+
+// Translate pixel ID into array coordinate
+function toCoordinate(id_num) {
+    id_num = parseInt(id_num);
+    var x = Math.floor(id_num/16);
+    var y = id_num-(16*x);
+    return [x, y-1];
+}
+
+// Translate array coordinate into pixel ID.
+function toID(x, y) {
+    return (16*x)+(y+1);
+}
+
+// Set listening events for pixels
+var pixelCells = document.getElementsByClassName("grid");
+for (k=0; k<pixelCells.length; k++) {
+    pixelCells[k].onclick = togglePixel;
+}
+
+// Select or deselect pixel and update pixel array
+function togglePixel() {
+    var pixelID = this.id;
+    var pixelCoordinate = toCoordinate(pixelID);
+    var pixelCell = document.getElementById(pixelID);
+
+    if (pixelCell.style.backgroundColor == "black") {
+        pixels[pixelCoordinate[0]][pixelCoordinate[1]] = "0";
+        pixelCell.style.backgroundColor = "white";
+    }
+    else {
+        pixels[pixelCoordinate[0]][pixelCoordinate[1]] = "1";
+        pixelCell.style.backgroundColor = "black";
+    }
+}
